6b714518a9ec958b93edfad572780360e9115578
timdd2100 <timdd2100@gmail.com>
2014-02-08 23:05:03 +0800

    splitview ex
    練習分割view

---
+++ splitViewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  splitViewTest
+//
+//  Created by Kao Chao-Ting on 2014/2/8.
+//  Copyright (c) 2014年 tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ splitViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  splitViewTest
+//
+//  Created by Kao Chao-Ting on 2014/2/8.
+//  Copyright (c) 2014年 tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
